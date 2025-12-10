745857273dcea2475cde9cf12196dfecd46c4b35
Bryan Wallace <36346389+brydenver2@users.noreply.github.com>
2025-12-09 18:05:42 -0700

    Update entrypoint.sh

--- pgpool/bin/entrypoint.sh
+++ pgpool/bin/entrypoint.sh
@@ -339,9 +339,10 @@ backend_flag$NUM = '$FLAG'
 done
 cat <<EOF >> $CONFIG_FILE
 # - Authentication -
-enable_pool_hba = off
+enable_pool_hba = on
 pool_hba_file = '/etc/pgpool-II/pool_hba.conf'
 pool_passwd = 'pool_passwd'
+allow_clear_text_frontend_auth = on
 authentication_timeout = 60
 # - SSL Connections -
 ssl = on
